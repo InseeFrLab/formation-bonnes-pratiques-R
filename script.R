00d171a0dc5e2056e941972f9067cf529f438d41
linogaliana <linogaliana@yahoo.fr>
2022-11-03 09:41:57 +0000

    premierl seignes

---
+++ script.R
@@ -0,0 +1,3 @@
+
+df <- readr::read_csv("/home/onyxia/formation-bonnes-pratiques-R/individu_reg.csv", col_names = c("region", "aemm", "aged", "ager20", "anai", "epci", "metrodom", "moco", "modv", "na38", "na88", "naf08", "sexe"))
+
