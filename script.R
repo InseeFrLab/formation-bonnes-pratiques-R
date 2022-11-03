a86ea22a1935046f856a3e253023b4831ecbfb20
linogaliana <linogaliana@yahoo.fr>
2022-11-03 13:53:46 +0000

    les install

--- script.R
+++ script.R
@@ -1,3 +1,11 @@
+rm(list = ls())
+
+if (!require('ggplot2')) install.packages('ggplot2')
+if (!require('stringr')) install.packages('stringr')
+if (!require('dplyr')) install.packages('dplyr')
+if (!require('tidyverse')) install.packages('tidyverse')
+
+
 library(tidyverse)
 library(dplyr)
 
