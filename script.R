1361b74131058eedc7df43b506d295f8e9d86ea4
linogaliana <linogaliana@yahoo.fr>
2022-11-04 16:48:00 +0000

    deduplique

--- script.R
+++ script.R
@@ -9,6 +9,7 @@ if (!require("MASS")) install.packages("MASS")
 
 library(tidyverse)
 library(dplyr)
+library(rlang)
 library(forcats)
 library(MASS)
 
@@ -33,10 +34,17 @@ stats_agregees <- function(a, b = "moyenne",
   )
   
 }
-stats_agregees(rnorm(10))
+
+#stats_agregees(rnorm(10))
 #stats_agregees(rnorm(10), "cart type")
-stats_agregees(rnorm(10), "ecart type")
-stats_agregees(rnorm(10), "variance")
+#stats_agregees(rnorm(10), "ecart type")
+#stats_agregees(rnorm(10), "variance")
+
+recode_as_na <- function(df, var_name, value){
+  df |>
+    mutate({{ var_name }} := na_if(!!rlang::sym(var_name), value))
+}
+
 
 
 
@@ -53,25 +61,24 @@ df2 <- readr::read_csv2(
 
 # TRAITEMENT VALEURS MANQUANTES ==================
 
-
-df2[df2$na38 == "ZZ", "na38"] <- NA
-df2[df2$trans == "Z", "trans"] <- NA
-df2[df2$tp == "Z", "tp"] <- NA
+df2 <- recode_as_na(df2, "na38", "ZZ")
+df2 <- recode_as_na(df2, "trans", "Z")
+df2 <- recode_as_na(df2, "tp", "Z")
 df2[endsWith(df2$naf08, "ZZ"), "naf08"] <- NA
 
 
 # TYPES EN FACTEUR ===================
 
-df2 <- df2 %>%
+df2 <- df2 |>
   mutate(across(
     c(-region, -aemm, -aged, -anai),
     as.factor)
   )
 
-df2 <- df2 %>%
+df2 <- df2 |>
   mutate(age = as.numeric(aged))
 
-df2$sexe <- df2$sexe %>%
+df2$sexe <- df2$sexe |>
   fct_recode(Homme = "1", Femme = "2")
 
 
@@ -93,8 +100,8 @@ print(summarise(df2, length(unique(unlist(cs1[!is.na(cs1)])))))
 summarise(group_by(df2, age), n())
 
 
-df2 %>%
-  dplyr::select(age) %>%
+df2 |>
+  dplyr::select(age) |>
   ggplot(.) + geom_histogram(aes(x = 5 * floor(age / 5)),
                              stat = "count")
 
@@ -109,74 +116,65 @@ ggplot(df2[as.numeric(df2$aged) > 50,],
 
 # part d'homme dans chaque cohorte ===================
 
-temp <- df2 %>%
-  group_by(age, sexe) %>%
-  summarise(SH_sexe = n()) %>%
-  group_by(age) %>%
-  mutate(SH_sexe = SH_sexe / sum(SH_sexe)) %>%
-  dplyr::filter(sexe == "Homme")
+part_total <- function(df2, var_groupe = "age", var_interet = "sexe"){
+  df2 |>
+    group_by(!!!syms(c(var_groupe, var_interet))) |>
+    summarise(share = n()) |>
+    group_by(!!sym(var_groupe)) |>
+    mutate(share = share / sum(share))
+}
+
+temp <- part_total(df2) |> filter(sexe=="Homme")
 
 ggplot(temp) +
   geom_bar(aes(x = as.numeric(age),
-               y = SH_sexe), stat = "identity") +
+               y = share), stat = "identity") +
   geom_point(aes(x = as.numeric(age),
-                 y = SH_sexe), stat = "identity", color = "red") +
+                 y = share), stat = "identity", color = "red") +
   coord_cartesian(c(0, 100))
 
 
 # stats surf par statut ==================
 
-df3 <- df2 |>
-  group_by(couple, surf) %>%
-  summarise(x = n()) %>%
-  group_by(couple) |>
-  mutate(y = 100 * x / sum(x))
+df3 <- part_total(df2, "couple", "surf")
 
 ggplot(df3) +
-  geom_bar(aes(x = surf, y = y, color = couple),
+  geom_bar(aes(x = surf, y = share, color = couple),
            stat = "identity", position = "dodge")
 
 # stats trans par statut ===================
 
-df3 <- df2 |>
-  group_by(couple, trans) %>%
-  summarise(x = n()) %>%
-  group_by(couple) |>
-  mutate(y = 100 * x / sum(x))
+df3 <- part_total(df2, "couple", "trans")
 
-ggplot(df3) + geom_bar(aes(x = trans, y = y, color = couple),
+ggplot(df3) + geom_bar(aes(x = trans, y = share, color = couple),
                        stat = "identity", position = "dodge")
 
 
 # STATS AGREGEES =================
 
-stats_agregees(df2 %>%
-                 filter(sexe == "Homme") %>%
-                 mutate(aged = as.numeric(aged)) %>%
-                 pull(aged), na.rm = TRUE)
-stats_agregees(df2 %>%
-                 filter(sexe == "Femme") %>%
-                 mutate(aged = as.numeric(aged)) %>%
-                 pull(aged), na.rm = TRUE)
-stats_agregees(df2 %>%
-                 filter(sexe == "Homme" & couple == "2") %>%
-                 mutate(aged = as.numeric(aged)) %>%
-                 pull(aged), na.rm = TRUE)
-stats_agregees(df2 %>%
-                 filter(sexe == "Femme" & couple == "2") %>%
-                 mutate(aged = as.numeric(aged)) %>%
-                 pull(aged), na.rm = TRUE)
+df3_homme <- df2 |>
+  filter(sexe == "Homme")
+df3_femme <- df2 |>
+  filter(sexe != "Homme")
+df3_homme_couple <- df3_homme |> filter(couple == 2)
+df3_femme_couple <- df3_femme |> filter(couple == 2)
+
+
+stats_agregees(df3_homme$age, na.rm = TRUE)
+stats_agregees(df3_femme$age, na.rm = TRUE)
+stats_agregees(df3_homme_couple$age, na.rm = TRUE)
+stats_agregees(df3_femme_couple$age, na.rm = TRUE)
 
 
 # MODELISATION ----------------------------
 
 
-df3 <- df2 %>%
-  dplyr::select(surf, cs1, ur, couple, age) %>%
+df3 <- df2 |>
+  dplyr::select(surf, cs1, ur, couple, age) |>
   filter(surf != "Z")
 
 polr(surf ~ cs1 + factor(ur),
-     df3 %>%
+     df3 |>
        filter(
          couple == 2 &
            age > 40 &
