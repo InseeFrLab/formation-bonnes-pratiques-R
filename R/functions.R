8d42730279619fb85a818e5fbde7176096915c94
linogaliana <linogaliana@yahoo.fr>
2022-11-05 14:22:42 +0000

    roxygenise

--- R/functions.R
+++ R/functions.R
@@ -1,10 +1,29 @@
 decennie_a_partir_annee <- function(annee) {
+  #' Transforme annee en decennie
+  #' 
+  #' @param annee Annee en question
+  #' @return La décennie en question
+  #' @example 
+  #' decennie_a_partir_annee(2011)
+  #' decennie_a_partir_annee(2010)
   return(annee - annee %% 10)
 }
-# fonction de stat agregee
-stats_agregees <- function(a, b = "moyenne",
+
+stats_agregees <- function(x, statistique = "moyenne",
                            ...) {
-  match.arg(b,
+  #' Statistiques agregees
+  #' 
+  #' @param statistique Statistique désirée. Les valeurs autorisées
+  #'  sont 'moyenne', 'variance', 'ecart-type', 'sd', 'ecart type'
+  #' @inheritParams mean
+  #' 
+  #' @examples
+  #' stats_agregees(rnorm(10))
+  #' stats_agregees(rnorm(10), "cart type")
+  #' stats_agregees(rnorm(10), "ecart type")
+  #' stats_agregees(rnorm(10), "variance")
+  
+  match.arg(statistique,
             c("moyenne",
               "variance",
               "ecart-type",
@@ -12,20 +31,24 @@ stats_agregees <- function(a, b = "moyenne",
               "ecart type")
   )
   
-  switch(b,
-         moyenne = mean(a, ...),
-         variance = var(a, ...),
-         sd(a, ...)
+  switch(statistique,
+         moyenne = mean(x, ...),
+         variance = var(x, ...),
+         sd(x, ...)
   )
   
 }
 
-#stats_agregees(rnorm(10))
-#stats_agregees(rnorm(10), "cart type")
-#stats_agregees(rnorm(10), "ecart type")
-#stats_agregees(rnorm(10), "variance")
+
 
 recode_as_na <- function(df, var_name, value){
+  #' Recode some values as NAs
+  #' 
+  #' @param df `data.frame` we should start from
+  #' @param var_name Variable name
+  #' @param value Value that should be replaced
+  #' @seealso dyplr::na_if
+
   df |>
     mutate({{ var_name }} := na_if(!!rlang::sym(var_name), value))
 }
