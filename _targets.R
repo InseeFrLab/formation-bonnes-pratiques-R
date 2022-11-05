05809d5a0a14a4b2e22bb8cc1a88b3d130800a14
linogaliana <linogaliana@yahoo.fr>
2022-11-05 17:21:19 +0000

    retour du sample

--- _targets.R
+++ _targets.R
@@ -17,16 +17,16 @@ tar_option_set(
 
 
 path_secrets <- "secrets.yaml"
-path_parquet_s3 <- "/diffusion/RP/2016/individu_reg/individu_reg.parquet"
-bucket_s3 <- "donnees-insee"
+path_parquet <- "individu_reg.parquet"
+
 
 
 list(
   tar_target(secrets_file, path_secrets, format = "file"),
+  tar_target(parquet_survey_file, path_parquet, format = "file"),
   tar_target(api_pwd, import_yaml(secrets_file)),
   tar_target(survey_sample_24,
-             import_parquet_raw(
-               path_parquet_s3,
-               bucket_s3),
+             import_from_parquet(
+               parquet_survey_file),
              format = "parquet")
 )
