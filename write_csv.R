a790300971ea14e1c84e9b3c13711ff6b52207ab
avouacr <avouacr@pm.me>
2022-11-13 21:14:52 +0000

    pre subset on s3

--- write_csv.R
+++ write_csv.R
@@ -1,13 +1,23 @@
-df_parquet <- 
+# df_parquet <- 
+#   aws.s3::s3read_using(
+#     FUN = arrow::read_parquet,
+#     object = "/diffusion/RP/2016/individu_reg/individu_reg.parquet",
+#     bucket = "donnees-insee",
+#     opts = list("region" = "")
+#   )
+
+# df_parquet
+
+# data.table::setDT(df_parquet)
+
+# data.table::fwrite(df_parquet[region == "24"], "individu_reg.csv", sep = ";")
+
+df <- 
   aws.s3::s3read_using(
-    FUN = arrow::read_parquet,
-    object = "/diffusion/RP/2016/individu_reg/individu_reg.parquet",
-    bucket = "donnees-insee",
+    FUN = readr::read_csv2,
+    object = "diffusion/bonnes-pratiques-r/rp_2016_individu_reg_24.csv",
+    bucket = "projet-formation",
     opts = list("region" = "")
   )
 
-df_parquet
-
-data.table::setDT(df_parquet)
-
-data.table::fwrite(df_parquet[region == "24"], "individu_reg.csv", sep = ";")
+readr::write_csv2(df, "individu_reg.csv")
