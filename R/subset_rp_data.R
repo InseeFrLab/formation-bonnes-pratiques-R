fca8916073bfa9c2e4172a4cb8be6a244b7c0a77
avouacr <avouacr@pm.me>
2022-11-21 15:09:56 +0000

    reorganize repo

---
+++ R/subset_rp_data.R
@@ -0,0 +1,14 @@
+
+df_parquet <- 
+  aws.s3::s3read_using(
+    FUN = arrow::read_parquet,
+    object = "/diffusion/RP/2016/individu_reg/individu_reg.parquet",
+    bucket = "donnees-insee",
+    opts = list("region" = "")
+  )
+
+df_parquet
+
+data.table::setDT(df_parquet)
+
+data.table::fwrite(df_parquet[region == "24"], "individu_reg.csv", sep = ";")
