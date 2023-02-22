afc66b0f4daccef6624e5457480efd1860870b67
lgaliana <lino.galiana@insee.fr>
2023-02-22 14:24:16 +0000

    appli 5

--- R/checkpoints/application5/_targets.R
+++ R/checkpoints/application5/_targets.R
@@ -1,10 +1,15 @@
+# _targets.R file
 library(targets)
 source("R/functions.R")
-tar_option_set(packages = c("dplyr", "forcats", "MASS", "yaml", "gt"))
+tar_option_set(packages = c("arrow","dplyr", "ggplot2", "forcats", "gt"))
+
+source("R/functions.R", encoding = "UTF-8")
+
 list(
-  tar_target(file_data, "individu_reg.parquet", format = "file"),
   tar_target(file_token, "secrets.yaml", format = "file"),
+  tar_target(file_data, "individu_reg.parquet", format = "file"),
+  tar_target(token, read_yaml_secret(file_token)),
   tar_target(data, read_from_parquet(file_data), format = "parquet"),
-  tar_target(token, read_yaml_secret(file_token, key = "JETON_API")),
-  tar_target(table_age, produce_table_age(data))
+  tar_target(clean_data, retraitement_donnees(data)),
+  tar_target(table_age, produce_table_age(clean_data))
 )
--- R/checkpoints/application5/functions.R
+++ R/checkpoints/application5/functions.R
@@ -1,45 +1,10 @@
-
 library(dplyr)
+library(ggplot2)
 library(forcats)
-library(MASS)
-library(yaml)
-library(gt)
-
-decennie_a_partir_annee <- function(annee) {
-  return(annee - annee %% 10)
-}
-
-#' Compute aggregated statistics
-#'
-#' @param a A numeric vector of values.
-#' @param b A string. The name of the statistic to compute.
-#' @return A number.
-#' @examples
-#' stats_agregees(rnorm(10))
-#' stats_agregees(rnorm(10), "ecart-type")
-#' stats_agregees(rnorm(10), "variance")
-stats_agregees <- function(a, b = "moyenne",
-                           ...) {
-  match.arg(b,
-            c("moyenne",
-              "variance",
-              "ecart-type",
-              "sd",
-              "ecart type")
-  )
-  
-  switch(b,
-         moyenne = mean(a, ...),
-         variance = var(a, ...),
-         sd(a, ...)
-  )
-  
-}
 
 read_yaml_secret <- function(path, key) {
   return(yaml::read_yaml(path)[[key]])
 }
-
 read_from_parquet <- function(path) {
   df <- arrow::read_parquet(
     path,
@@ -52,31 +17,69 @@ read_from_parquet <- function(path) {
   return(df)
 }
 
-produce_table_age <- function(df) {
+decennie_a_partir_annee <- function(annee) {
+  return(annee - annee %% 10)
+}
 
+#' Calcul automatique de stats agrégées
+#'
+#' @param x Un vecteur
+#' @param stat La statistique d'intérêt. Peut-être "moyenne", "écart-type"
+#'  ou "variance". Par défaut "moyenne"
+#' @param ...  Arguments additionnels à passer aux fonctions de stats agrégées
+#'
+#' @return Un vecteur avec la statistique d'intérêt
+#' @export
+#'
+#' @examples
+#' fonction_de_stat_agregee(rnorm(10))
+#' fonction_de_stat_agregee(rnorm(10), "ecart-type")
+#' fonction_de_stat_agregee(rnorm(10), "variance")
+stats_agregees <- function(x, stat = "moyenne", ...) {
+  if (stat == "moyenne") {
+    resultat <- mean(x, na.rm = TRUE, ...)
+  } else if (stat == "ecart-type" || stat == "sd") {
+    resultat <- sd(x, na.rm = TRUE, ...)
+  } else if (stat == "variance") {
+    resultat <- var(x, na.rm = TRUE, ...)
+  }
+  return(resultat)
+}
+
+
+
+retraitement_donnees <- function(df){
+  df <- df %>%
+    mutate(aged = as.numeric(aged))
+  df$sexe <- df$sexe %>%
+    as.character() %>%
+    fct_recode(Homme = "1", Femme = "2")
+  df <- df %>%
+    mutate(
+      surf = factor(surf, ordered = TRUE),
+      cs1 = factor(cs1)
+    )
+  return(df)
+}
+
+
+produce_table_age <- function(df){
   stats_age <- df %>%
-    mutate(age = as.numeric(aged)) %>%
-    group_by(decennie = decennie_a_partir_annee(age)) %>%
-    summarise(n_indiv = n(),
-              n_femmes = sum(sexe == "Femme"),
-              n_hommes = sum(sexe == "Homme")
-              )
+    group_by(decennie = decennie_a_partir_annee(aged)) %>%
+    summarise(n())
   
-  table_age <- gt(stats_age) %>%
-    tab_header(
-      title = "Distribution de la population par décennie"
+  table_age <- gt::gt(stats_age) %>%
+    gt::tab_header(
+      title = "Distribution des âges dans notre population"
     ) %>%
-    fmt_number(
-      columns = n_indiv,
+    gt::fmt_number(
+      columns = `n()`,
       sep_mark = " ",
       decimals = 0
     ) %>%
-    cols_label(
+    gt::cols_label(
       decennie = "Tranche d'âge",
-      n_indiv = "Population",
-      n_femmes = "Nombre de femmes",
-      n_hommes = "Nombre d'hommes"
+      `n()` = "Population"
     )
-  
   return(table_age)
 }
